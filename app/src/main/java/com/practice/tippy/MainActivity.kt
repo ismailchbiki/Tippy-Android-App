6760affbce02268f76a8509aea6149aaafe04140
Ismail Chbiki <ismailchbiki.nl@gmail.com>
2022-08-05 22:22:01 +0200

    Tippy Android App finalized

--- app/src/main/java/com/practice/tippy/MainActivity.kt
+++ app/src/main/java/com/practice/tippy/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.practice.tippy
 
+import android.animation.ArgbEvaluator
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -8,6 +10,7 @@ import android.util.Log
 import android.widget.EditText
 import android.widget.SeekBar
 import android.widget.TextView
+import androidx.core.content.ContextCompat
 
 private const val TAG = "MainActivity" //creating a tag to filter by name in the console log
 private const val INITIAL_TIP_PERCENT = 15
@@ -18,6 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTipPercentLbl: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,15 +31,18 @@ class MainActivity : AppCompatActivity() {
         tvTipPercentLbl = findViewById(R.id.tvTipPercentLbl)
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        tvTipDescription = findViewById(R.id.tvTipDescription)
 
         seekBarTip.progress = INITIAL_TIP_PERCENT
         tvTipPercentLbl.text = "$INITIAL_TIP_PERCENT%"
+        updateTipDescription(INITIAL_TIP_PERCENT)
         seekBarTip.setOnSeekBarChangeListener(object :
             SeekBar.OnSeekBarChangeListener { // We define an anonymous class "object", which implements in interface "OnSeekBarChangeListener"
             override fun onProgressChanged(p0: SeekBar?, progress: Int, p2: Boolean) {
                 Log.i(TAG, "onProgressChanged $progress") //equivalent to "console.log() in JS"
                 tvTipPercentLbl.text = "$progress%"
                 computeTipAndTotal()
+                updateTipDescription(progress)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {}
@@ -52,6 +59,36 @@ class MainActivity : AppCompatActivity() {
         })
     }
 
+    private fun updateTipDescription(tipPercent: Int) {
+        // update the text based on the tipPercent
+        val tipDescription = when (tipPercent) {
+            in 0..9 -> "Poor"
+            in 10..14 -> "Acceptable"
+            in 15..19 -> "Good"
+            in 20..24 -> "Great"
+            else -> "Amazing"
+        }
+        tvTipDescription.text = tipDescription
+        // update the color based on the tipPercent
+        //First technique - Very Basic
+//        val tipDescriptionColor = when (tipDescription) {
+//            "Poor" -> Color.DKGRAY
+//            "Acceptable" -> Color.GRAY
+//            "Good" -> Color.BLUE
+//            "Great" -> Color.CYAN
+//            else -> Color.GREEN
+//        }
+//        tvTipDescription.setTextColor(tipDescriptionColor)
+
+        //Second technique - with maths
+        val color = ArgbEvaluator().evaluate(
+            tipPercent.toFloat() / seekBarTip.max,
+            ContextCompat.getColor(this, R.color.color_worstTip),
+            ContextCompat.getColor(this, R.color.color_bestTip)
+        ) as Int
+        tvTipDescription.setTextColor(color)
+    }
+
     private fun computeTipAndTotal() {
         if (etBaseAmount.text.isEmpty()) {
             tvTipAmount.text
@@ -66,7 +103,9 @@ class MainActivity : AppCompatActivity() {
         val tipAmount = baseAmount * tipPercent / 100
         val totalAmount = baseAmount + tipAmount
         // 3. Update the UI
-        tvTipAmount.text = "%.2f".format(tipAmount).toString() //formatting the number (cutting off the long decimals)
+        tvTipAmount.text =
+            "%.2f".format(tipAmount)
+                .toString() //formatting the number (cutting off the long decimals)
         tvTotalAmount.text = "%.2f".format(totalAmount).toString()
     }
 }
